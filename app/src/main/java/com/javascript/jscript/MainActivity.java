8f1c0e423d596d631449d08fe91341b7b282a57d
shrCreation <shrhossain786@gmail.com>
2021-11-22 19:32:37 +0600

    status bar color change

--- app/src/main/java/com/javascript/jscript/MainActivity.java
+++ app/src/main/java/com/javascript/jscript/MainActivity.java
@@ -2,6 +2,8 @@ package com.javascript.jscript;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.javascript.jscript.databinding.ActivityMainBinding;
 
 
@@ -12,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //change status bar color
-        //getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this,R.color.main_background_color));
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this,R.color.colorStatusBarDark));
         //binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());

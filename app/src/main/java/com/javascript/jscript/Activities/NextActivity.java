c6df6d325dd0ce64f9306979bcd82cf0534da0aa
shrCreation <shrhossain786@gmail.com>
2021-11-23 17:11:17 +0600

    splashActivityFinished-v1.1.1.5

---
+++ app/src/main/java/com/javascript/jscript/Activities/NextActivity.java
@@ -0,0 +1,16 @@
+package com.javascript.jscript.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.javascript.jscript.R;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
--- app/src/main/java/com/javascript/jscript/Activities/SplashActivity.java
+++ app/src/main/java/com/javascript/jscript/Activities/SplashActivity.java
@@ -3,13 +3,24 @@ package com.javascript.jscript.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.javascript.jscript.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    //Variables
+    private static int SPLASH_SCREEN = 400000;
+    Animation topAmin,bottomAnim;
+    ImageView image;
+    TextView slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +29,23 @@ public class SplashActivity extends AppCompatActivity {
         //change status bar color
         getWindow().setStatusBarColor(ContextCompat.getColor(SplashActivity.this, R.color.colorStatusBarDark));
         setContentView(R.layout.activity_splash);
+        //animations
+        topAmin = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //hooks
+        image = findViewById(R.id.splash_logo);
+        slogan = findViewById(R.id.slogan_text);
+        //set animations
+        image.setAnimation(topAmin);
+        slogan.setAnimation(bottomAnim);
+        //handler for change activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,NextActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }

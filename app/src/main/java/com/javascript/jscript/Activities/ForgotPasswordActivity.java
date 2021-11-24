2361ea96511b71fac6018eae7e8af1b67684cd8d
shrCreation <shrhossain786@gmail.com>
2021-11-24 15:22:03 +0600

    ForgotPasswordActivityFinished -v1.2.5.16

--- app/src/main/java/com/javascript/jscript/Activities/ForgotPasswordActivity.java
+++ app/src/main/java/com/javascript/jscript/Activities/ForgotPasswordActivity.java
@@ -3,17 +3,70 @@ package com.javascript.jscript.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.javascript.jscript.R;
+import com.javascript.jscript.databinding.ActivityForgotPasswordBinding;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    //binding
+    ActivityForgotPasswordBinding binding;
+    TextInputLayout textInputLayoutEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //change status bar color
         getWindow().setStatusBarColor(ContextCompat.getColor(ForgotPasswordActivity.this, R.color.activeGreen));
-        setContentView(R.layout.activity_forgot_password);
+        binding = ActivityForgotPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        textInputLayoutEmail = findViewById(R.id.text_input_email);
+
+        binding.forgotPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check first email validation
+                validateEmail();
+                //other codes
+
+            }
+        });
+
+        binding.goToSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPasswordActivity.this,LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+    //others methods
+    //email Validation
+    private boolean validateEmail() {
+        String emailInput = textInputLayoutEmail.getEditText().getText().toString().trim();
+        if (emailInput.isEmpty()) {
+            textInputLayoutEmail.setError("Field can't be empty");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            textInputLayoutEmail.setError("Please enter a valid email address");
+            return false;
+        } else {
+            textInputLayoutEmail.setError(null);
+            //textInputEmail.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+
+
 }

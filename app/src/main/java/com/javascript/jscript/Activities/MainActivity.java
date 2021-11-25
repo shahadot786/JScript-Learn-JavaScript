90900df046da2ff89a7b9f1444591b68ca2fe79d
shrCreation <shrhossain786@gmail.com>
2021-11-25 23:20:56 +0600

    main activity done with bottom navigation v-1.3.6.25

--- app/src/main/java/com/javascript/jscript/Activities/MainActivity.java
+++ app/src/main/java/com/javascript/jscript/Activities/MainActivity.java
@@ -1,13 +1,18 @@
 package com.javascript.jscript.Activities;
 
 import android.os.Bundle;
-import android.view.WindowManager;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.javascript.jscript.Fragment.DiscussFragment;
+import com.javascript.jscript.Fragment.FeedsFragment;
+import com.javascript.jscript.Fragment.LearnFragment;
+import com.javascript.jscript.Fragment.ProFragment;
+import com.javascript.jscript.Fragment.ProfileFragment;
 import com.javascript.jscript.R;
 import com.javascript.jscript.databinding.ActivityMainBinding;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +24,42 @@ public class MainActivity extends AppCompatActivity {
         //binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //by default fragment code
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new FeedsFragment());
+        transaction.commit();
+
+        //bottom bar fragment listener
+        binding.bottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                //fragment replace
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                //fragment replace switch case code
+                switch (i){
+                    case 0:
+                        transaction.replace(R.id.container, new FeedsFragment());
+                        break;
+                    case 1:
+                        transaction.replace(R.id.container, new LearnFragment());
+                        break;
+                    case 2:
+                        transaction.replace(R.id.container, new DiscussFragment());
+                        break;
+                    case 3:
+                        transaction.replace(R.id.container, new ProfileFragment());
+                        break;
+                    case 4:
+                        transaction.replace(R.id.container, new ProFragment());
+                        break;
+                }
+                transaction.commit();
+                return false;
+            }
+        });
+
+
+
     }
 }

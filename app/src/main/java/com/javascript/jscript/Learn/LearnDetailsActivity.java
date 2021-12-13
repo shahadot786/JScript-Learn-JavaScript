70a055b7c55eed40fb8305ff6106c9693ea0d049
shrCreation <shrhossain786@gmail.com>
2021-12-14 01:59:57 +0600

    start work with learn fragment

---
+++ app/src/main/java/com/javascript/jscript/Learn/LearnDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.javascript.jscript.Learn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.javascript.jscript.R;
+
+public class LearnDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_details);
+    }
+}

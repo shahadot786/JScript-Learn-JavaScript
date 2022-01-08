a54dcc5f7e70ce0387e46d2dcf264c5ef7fa7597
shrCreation <shrhossain786@gmail.com>
2022-01-09 01:31:26 +0600

    close keyboard

--- app/src/main/java/com/javascript/jscript/Discuss/DiscussDetailsActivity.java
+++ app/src/main/java/com/javascript/jscript/Discuss/DiscussDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.javascript.jscript.Discuss;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -11,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -235,6 +237,10 @@ public class DiscussDetailsActivity extends AppCompatActivity {
                                             @Override
                                             public void onSuccess(@NonNull Void unused) {
                                                 binding.commentET.setText("");
+                                                //hide keyboard
+                                                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                                                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                                                //notifications
                                                 NotificationsModel notifications = new NotificationsModel();
                                                 notifications.setNotificationBy(FirebaseAuth.getInstance().getUid());
                                                 notifications.setNotificationAt(new Date().getTime());

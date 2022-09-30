d4206c00fa6bfaa4fc0090983e3435f8f24c90f4
shrCreation <shrhossain786@gmail.com>
2022-09-30 19:38:22 +0600

    change to main branch

---
+++ app/build/generated/source/buildConfig/release/com/javascript/jscript/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.javascript.jscript;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.javascript.jscript";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 24;
+  public static final String VERSION_NAME = "1.4.15.24";
+}
